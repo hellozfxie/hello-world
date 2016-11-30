2ea9bdcfee835dab15903e2aa5fe6a4e0da2281e
hellozfxie <814045282@qq.com>
2016-11-30 11:23:13 +0800

    Signed-off-by: hellozfxie <814045282@qq.com>

--- src/main/java/sayHelloWorld.java
+++ src/main/java/sayHelloWorld.java
@@ -6,5 +6,6 @@ package main.java;
 public class sayHelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
